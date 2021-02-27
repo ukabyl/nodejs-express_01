0ea58f0b56bb8a7281857d20477d1267ca74cfa0
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2021-02-27 23:21:25 +0500

    Add express-handlebars&nodemon

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
+const hbs = exphbs.create({
+  extname: 'hbs',
+  defaultLayout: 'main'
+});
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+  res.render('about');
 });
 
 const PORT = process.argv.PORT | 3000;
