702580824900b21278fc12fe3d2fcdddd33f0aa3
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2021-02-27 23:00:32 +0500

    Sending hmtl file

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+const PORT = process.argv.PORT | 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is runnig ${PORT}`);
 });
