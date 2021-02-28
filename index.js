dd594c19c2bca8d5a06ed9332d19cede2a14afec
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2021-02-28 10:19:35 +0500

    Add navbar

--- index.js
+++ index.js
@@ -8,16 +8,31 @@ const hbs = exphbs.create({
   defaultLayout: 'main'
 });
 
-app.engine('hbs', hbs.engine)
+app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Home',
+    isHome: true
+  });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'Courses',
+    isCourses: true
+  });
+});
+
+app.get('/add-course', (req, res) => {
+  res.render('addCourse', {
+    title: 'Add course',
+    isAddCourse: true
+  });
 });
 
 const PORT = process.argv.PORT | 3000;
