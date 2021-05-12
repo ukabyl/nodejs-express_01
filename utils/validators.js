6bce0773ef45acedc7aad73ed1330384c61e0c1f
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2021-05-12 20:56:18 +0500

    Normalize data from form

--- utils/validators.js
+++ utils/validators.js
@@ -3,14 +3,19 @@ const { Promise } = require('mongoose');
 const User = require('../models/user');
 
 exports.registerValidations = [
-  body('email').isEmail().withMessage('Email has an incorrect value').custom(async (email) => {
-    const user = await User.findOne({ email });
-    if (user) {
-      return Promise.reject('User already exists');
-    }
-  }),
-  body('name').isLength({min: 3}).withMessage('Name should have minimum 3 characters'),
-  body('password', 'Password should have minimum 6 characters').isLength({ min: 6, max: 56 }).isAlphanumeric().withMessage('Password should have numbers and letters'),
+  body('email').isEmail().withMessage('Email has an incorrect value')
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        return Promise.reject('User already exists');
+      }
+    })
+    .trim(),
+  body('name')
+    .isLength({min: 3}).withMessage('Name should have minimum 3 characters')
+    .trim(),
+  body('password', 'Password should have minimum 6 characters').isLength({ min: 6, max: 56 })
+  .isAlphanumeric().trim(),
   body('confirm').custom((value, {req}) => {
     if (value !== req.body.password) {
       throw new Error('Password and confirm don\'t match')
