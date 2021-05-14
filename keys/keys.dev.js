8d07d551c405b6e318437d562f822c0d2a956962
Umirzak Kabylbekov <umkabylbekov@gmail.com>
2021-05-14 22:50:10 +0500

    Separate keys for prod & dev mode

---
+++ keys/keys.dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGO_URI: `mongodb+srv://ukabyl:${process.env.PASSWORD}@cluster0.xpxu8.mongodb.net/db`,
+  SESSION_SECRET: 'secret value',
+  SENDGRID_API_KEY: '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',
+  BASE_URL: 'http://localhost:3000'
+}
---
+++ keys/keys.prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGO_URI: process.env.MONGO_URI,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  BASE_URL: process.env.BASE_URL,
+}
